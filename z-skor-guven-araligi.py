005d6c1a72e3f8e5b36b3595408b31aefd960c87
tatar <gokberktatar@gmail.com>
2023-06-13 19:18:32 +0300

    Fixed: standart hata hesaplanırken işlem hatası yapılmış. Düzeltildi.

--- z-skor-guven-araligi.py
+++ z-skor-guven-araligi.py
@@ -20,7 +20,7 @@ if z_score is None:
 mean = np.mean(data)
 
 # Standart hatayı hesapla
-standard_error = np.std(data) / np.sqrt(len(data))
+standard_error = 1 / np.sqrt(len(data))
 
 print(Fore.GREEN +"Standart hata: " + Fore.RESET, standard_error)
 
